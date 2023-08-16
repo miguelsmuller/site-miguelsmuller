48ef3fca48258419ca193e16cdcb4464d14e6ee3
Miguel Muller <miguel.muller@g.globo>
2023-08-16 11:10:36 -0300

    feat: disable service component from index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,7 +29,6 @@ export default function Home(props: any) {
 
       <Header refHero={refHero} />
       <Overview />
-      <Services />
       <Study />
       <Portifolio />
       <Testimonies />
