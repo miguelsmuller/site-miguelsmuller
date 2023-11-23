bcc3b49f92836531270eaa88ba5f5b3b78892285
Miguel Muller <miguelsmuller@users.noreply.github.com>
2023-11-23 20:18:09 -0300

    refactor: update nextjs handler for firebase function

--- server.js
+++ server.js
@@ -2,37 +2,32 @@ const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const next = require('next');
 
+// Inicialização do Firebase Admin SDK
 admin.initializeApp();
 
+// Verificação do ambiente de execução
 const env = process.env.NODE_ENV !== 'production';
+
+// Carregamento da configuração do Next.js
 const config = require('./next.config');
+
+// Configuração do aplicativo Next.js
 const app = next({
-    dev: env,
-    conf: config,
+  dev: env,
+  conf: config,
 });
+
+// Manipulador de requisições Next.js
 const handle = app.getRequestHandler();
 
+// Função Cloud Function para manipular requisições HTTP
 exports.nextjsServer = functions.https.onRequest((request, response) => {
-    console.log('File: ' + request.originalUrl);
-    return app.prepare().then(() => handle(request, response));
+  console.log(`File: ${request.originalUrl}`);
+
+  return app.prepare()
+    .then(() => handle(request, response))
+    .catch((error) => {
+      console.error('Error during request processing:', error);
+      response.status(500).send('Internal server error');
+    });
 });
-
-
-// const functions  = require('firebase-functions')
-// const { default: next } = require('next')
-
-// const config = require("./next.config");
-// const dev = process.env.NODE_ENV !== "production";
-
-// const app = next({
-// 	dev: dev,
-// 	conf: config,
-// });
-// const handle = app.getRequestHandler()
-
-// const server = functions.https.onRequest((req, res) => {
-// 	console.log("File: " + req.originalUrl);
-// 	return app.prepare().then(() => handle(req, res));
-// });
-
-// exports.nextjs = { server };
